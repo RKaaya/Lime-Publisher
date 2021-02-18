01bcdf99e85a514929e2708db2b6b5a927a9a5af
Rkaaya <gergelynagy99@gmail.com>
2021-02-18 23:07:28 +0100

    init

---
+++ src/main/java/com/rkaaya/limepublisher/LimePublisherApplication.java
@@ -0,0 +1,13 @@
+package com.rkaaya.limepublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimePublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimePublisherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rkaaya/limepublisher/LimePublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkaaya.limepublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimePublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
